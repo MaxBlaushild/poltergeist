74b1e6d8ea6ebe87979eaf4a8a7ba27d2cf2f9c2
Max Blaushild <max@turnkey.io>
2024-08-26 19:45:21 -0400

    foo

--- go/migrate/internal/migrations/000053_remove_inventory_item_foreign_key.up.sql
+++ go/migrate/internal/migrations/000053_remove_inventory_item_foreign_key.up.sql
@@ -1,7 +1,11 @@
 ALTER TABLE team_inventory_items
-  ALTER COLUMN inventory_item_id TYPE int USING (inventory_item_id::int);
+  DROP COLUMN inventory_item_id;
+ALTER TABLE team_inventory_items
+  ADD COLUMN inventory_item_id int;
 CREATE INDEX idx_team_inventory_items_inventory_item_id ON team_inventory_items (inventory_item_id);
 
 ALTER TABLE match_inventory_item_effects
-  ALTER COLUMN inventory_item_id TYPE int USING (inventory_item_id::int);
+  DROP COLUMN inventory_item_id;
+ALTER TABLE match_inventory_item_effects
+  ADD COLUMN inventory_item_id int;
 CREATE INDEX idx_match_inventory_item_effects_inventory_item_id ON match_inventory_item_effects (inventory_item_id);
