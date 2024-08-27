508e89647ec4b89664090d403180218d05b6db90
Max Blaushild <max@turnkey.io>
2024-08-26 21:07:45 -0400

    fix

--- go/migrate/internal/migrations/000054_add_team_id_to_match_inventory_items.down.sql
+++ go/migrate/internal/migrations/000054_add_team_id_to_match_inventory_items.down.sql
@@ -1,5 +1,5 @@
-ALTER TABLE match_inventory_items
+ALTER TABLE match_inventory_item_effects
 DROP CONSTRAINT fk_team_id;
 
-ALTER TABLE match_inventory_items
+ALTER TABLE match_inventory_item_effects
 DROP COLUMN team_id;
--- go/migrate/internal/migrations/000054_add_team_id_to_match_inventory_items.up.sql
+++ go/migrate/internal/migrations/000054_add_team_id_to_match_inventory_items.up.sql
@@ -1,6 +1,6 @@
-ALTER TABLE match_inventory_items
+ALTER TABLE match_inventory_item_effects
 ADD COLUMN team_id UUID;
 
-ALTER TABLE match_inventory_items
+ALTER TABLE match_inventory_item_effects
 ADD CONSTRAINT fk_team_id
 FOREIGN KEY (team_id) REFERENCES teams(id);
