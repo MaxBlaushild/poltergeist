e01735066d33a268e8fb30910eb2e4664ce30c17
Max Blaushild <max@turnkey.io>
2025-02-16 16:35:01 -0500

    new shit

--- go/migrate/internal/migrations/000064_add_users_to_audit_items.up.sql
+++ go/migrate/internal/migrations/000064_add_users_to_audit_items.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE audit_items
-    ALTER COLUMN team_id DROP NOT NULL,
+    ALTER COLUMN match_id DROP NOT NULL,
     ADD COLUMN user_id UUID,
     ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id);
