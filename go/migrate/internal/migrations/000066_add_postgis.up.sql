e0573dc800a3489ab4968d5d7b50177e481a074e
Max Blaushild <max@turnkey.io>
2025-03-08 17:39:17 -0500

    fix migration

--- go/migrate/internal/migrations/000066_add_postgis.up.sql
+++ go/migrate/internal/migrations/000066_add_postgis.up.sql
@@ -1,3 +1,3 @@
-CREATE EXTENSION postgis;
+CREATE EXTENSION IF NOT EXISTS postgis;
 
-ALTER TABLE point_of_interest ADD COLUMN geometry geometry(Point, 4326);
+ALTER TABLE points_of_interest ADD COLUMN geometry geometry(Point, 4326);
