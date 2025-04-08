b0a3d3d0c0b8e7d30658160b3751d32e4898cbbf
Max Blaushild <max@turnkey.io>
2025-04-07 20:18:23 -0400

    foo

--- go/migrate/internal/migrations/000071_add_tags_to_tag_entities.down.sql
+++ go/migrate/internal/migrations/000071_add_tags_to_tag_entities.down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE tag_entities
 DROP CONSTRAINT fk_tag_entities_tag;
+
+ALTER TABLE tag_entities
+DROP COLUMN tag_id;
--- go/migrate/internal/migrations/000071_add_tags_to_tag_entities.up.sql
+++ go/migrate/internal/migrations/000071_add_tags_to_tag_entities.up.sql
@@ -1,4 +1,7 @@
 ALTER TABLE tag_entities
+ADD COLUMN tag_id UUID;
+
+ALTER TABLE tag_entities
 ADD CONSTRAINT fk_tag_entities_tag
 FOREIGN KEY (tag_id) 
 REFERENCES tags(id);
