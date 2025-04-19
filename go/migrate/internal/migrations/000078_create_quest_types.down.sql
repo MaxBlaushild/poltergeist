db8172b65654cf6ffc2fa8917220e68e6836ccc9
Max Blaushild <max@turnkey.io>
2025-04-18 22:57:20 -0400

    fixy fix

--- go/migrate/internal/migrations/000078_create_quest_types.down.sql
+++ go/migrate/internal/migrations/000078_create_quest_types.down.sql
@@ -3,7 +3,7 @@ DROP INDEX IF EXISTS idx_quest_archtype_type_challenge_id;
 DROP INDEX IF EXISTS idx_quest_archtype_challenge_node_id;
 DROP INDEX IF EXISTS idx_quest_archtype_challenge_unlocked_node_id;
 
-DROP TABLE IF EXISTS QuestArchtypes;
-DROP TABLE IF EXISTS QuestArchTypeNodeChallenges;
-DROP TABLE IF EXISTS QuestArchtypeChallenges;
-DROP TABLE IF EXISTS QuestArchtypeNodes;
+DROP TABLE IF EXISTS quest_archetypes;
+DROP TABLE IF EXISTS quest_archetype_node_challenges;
+DROP TABLE IF EXISTS quest_archetype_challenges;
+DROP TABLE IF EXISTS quest_archetype_nodes;
